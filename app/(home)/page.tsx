646e6203c6f4e2614c4c88183675734a08d4e04c
Asef <asefhosseini00@gmail.com>
2025-05-15 16:29:15 +0330

    create a home group directory and move the page file to it

---
+++ app/(home)/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import HeroSection from './Slider';
+
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="w-full h-full">
+        <HeroSection />
+      </main>
+    </div>
+  );
+}
