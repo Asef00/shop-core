69088c6e7526df6a070f55ca1e44216a7a5b000d
Asef <asefhosseini00@gmail.com>
2025-05-19 13:53:34 +0330

    test: add basic footer template

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,416 @@
+export default function Footer() {
+  return (
+    <footer className="footer" id="footer">
+      <div className="footer-wrap border-0">
+        <div className="footer-body">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4">
+                <div className="footer-infor">
+                  <div className="footer-logo">
+                    <a href="index.html">
+                      <img alt="" src="images/logo/logo.svg" />
+                    </a>
+                  </div>
+                  <div className="footer-address">
+                    <p>549 Oak St.Crystal Lake ، IL 60014</p>
+                    <a className="tf-btn-default fw-6" href="contact.html">
+                      تماس با ما<i className="icon-arrowUpRight"></i>
+                    </a>
+                  </div>
+                  <ul className="footer-info">
+                    <li>
+                      <i className="icon-mail"></i>
+                      <p>themesflat@gmail.com</p>
+                    </li>
+                    <li>
+                      <i className="icon-phone"></i>
+                      <p>315-666-6688</p>
+                    </li>
+                  </ul>
+                  <ul className="tf-social-icon">
+                    <li>
+                      <a
+                        className="social-facebook"
+                        href="home-activewear.html#"
+                      >
+                        <i className="icon icon-fb"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a className="social-twiter" href="home-activewear.html#">
+                        <i className="icon icon-x"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="social-instagram"
+                        href="home-activewear.html#"
+                      >
+                        <i className="icon icon-instagram"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a className="social-tiktok" href="home-activewear.html#">
+                        <i className="icon icon-tiktok"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a className="social-amazon" href="home-activewear.html#">
+                        <i className="icon icon-amazon"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="social-pinterest"
+                        href="home-activewear.html#"
+                      >
+                        <i className="icon icon-pinterest"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="footer-menu">
+                  <div className="footer-col-block">
+                    <div className="footer-heading text-button footer-heading-mobile">
+                      اطلاعات
+                    </div>
+                    <div className="tf-collapse-content">
+                      <ul className="footer-menu-list">
+                        <li className="text-caption-1">
+                          <a className="footer-menu_item" href="about-us.html">
+                            درباره ما
+                          </a>
+                        </li>
+                        <li className="text-caption-1">
+                          <a
+                            className="footer-menu_item"
+                            href="home-activewear.html#"
+                          >
+                            داستان های ما
+                          </a>
+                        </li>
+                        <li className="text-caption-1">
+                          <a
+                            className="footer-menu_item"
+                            href="home-activewear.html#"
+                          >
+                            راهنمای اندازه
+                          </a>
+                        </li>
+                        <li className="text-caption-1">
+                          <a className="footer-menu_item" href="contact.html">
+                            با ما تماس بگیرید
+                          </a>
+                        </li>
+                        <li className="text-caption-1">
+                          <a
+                            className="footer-menu_item"
+                            href="home-activewear.html#"
+                          >
+                            شغلی
+                          </a>
+                        </li>
+                        <li className="text-caption-1">
+                          <a
+                            className="footer-menu_item"
+                            href="my-account.html"
+                          >
+                            حساب من
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="footer-col-block">
+                    <div className="footer-heading text-button footer-heading-mobile">
+                      خدمات مشتری
+                    </div>
+                    <div className="tf-collapse-content">
+                      <ul className="footer-menu-list">
+                        <li className="text-caption-1">
+                          <a
+                            className="footer-menu_item"
+                            href="home-activewear.html#"
+                          >
+                            حمل
+                          </a>
+                        </li>
+                        <li className="text-caption-1">
+                          <a
+                            className="footer-menu_item"
+                            href="home-activewear.html#"
+                          >
+                            بازگشت و بازپرداخت
+                          </a>
+                        </li>
+                        <li className="text-caption-1">
+                          <a
+                            className="footer-menu_item"
+                            href="home-activewear.html#"
+                          >
+                            سیاست حفظ حریم
+                          </a>
+                        </li>
+                        <li className="text-caption-1">
+                          <a
+                            className="footer-menu_item"
+                            href="term-of-use.html"
+                          >
+                            شرایط و ضوابط
+                          </a>
+                        </li>
+                        <li className="text-caption-1">
+                          <a className="footer-menu_item" href="FAQs.html">
+                            سؤالات متداول سفارشات
+                          </a>
+                        </li>
+                        <li className="text-caption-1">
+                          <a className="footer-menu_item" href="wish-list.html">
+                            لیست دلخواه من
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="footer-col-block">
+                  <div className="footer-heading text-button footer-heading-mobile">
+                    خبرنامه
+                  </div>
+                  <div className="tf-collapse-content">
+                    <div className="footer-newsletter">
+                      <p className="text-caption-1">
+                        برای خبرنامه ما ثبت نام کنید و 10 ٪ از اولین خرید خود را
+                        دریافت کنید
+                      </p>
+                      <div className="sib-form">
+                        <div
+                          className="sib-form-container"
+                          id="sib-form-container"
+                        >
+                          <div
+                            className="sib-form-message-panel"
+                            id="error-message"
+                          >
+                            <div className="sib-form-message-panel__text sib-form-message-panel__text--center">
+                              <span className="sib-form-message-panel__inner-text">
+                                اشتراک شما قابل ذخیره نبود.لطفا دوباره امتحان
+                                کنید
+                              </span>
+                            </div>
+                          </div>
+                          <div
+                            className="sib-form-message-panel"
+                            id="success-message"
+                          >
+                            <div className="sib-form-message-panel__text sib-form-message-panel__text--center">
+                              <span className="sib-form-message-panel__inner-text">
+                                اشتراک شما موفقیت آمیز بوده است.
+                              </span>
+                            </div>
+                          </div>
+                          <div
+                            className="sib-container--large sib-container--vertical"
+                            id="sib-container"
+                          >
+                            <form
+                              action="https://3c02c1a1.sibforms.com/serve/MUIFAOAhSCDRnPhdPWLTpLBkaFR0CvSbJ_okYrjCbXQRLkZZU67Hn2jdn18hTWJuGupI4VUfB4deuJIyP5yRoHWVb9pIrENAMcal9Jtz8q_qN4dpHNMIG454DwSVNVmnLXuePoOCvDqN_Vvs0ga_kzg7ouD63HjCaukRz3LGCQsfnQJBN4-KS2D3DVitqvFsDHSqevjjqLk2xFO4"
+                              className="form-newsletter"
+                              data-type="subscription"
+                              id="sib-form"
+                              method="POST"
+                            >
+                              <div>
+                                <div className="sib-form-block">
+                                  <p></p>
+                                </div>
+                              </div>
+                              <div>
+                                <div className="sib-form-block">
+                                  <div className="sib-text-form-block">
+                                    <p></p>
+                                  </div>
+                                </div>
+                              </div>
+                              <div>
+                                <div className="sib-input sib-form-block">
+                                  <div className="form__entry entry_block">
+                                    <div className="form__label-row">
+                                      <label
+                                        className="entry__label"
+                                        for="EMAIL"
+                                      ></label>
+                                      <div className="entry__field">
+                                        <input
+                                          autocomplete="off"
+                                          className="input radius-60"
+                                          data-required="true"
+                                          id="EMAIL"
+                                          name="EMAIL"
+                                          placeholder="ایمیل خود را وارد کنید ..."
+                                          required=""
+                                          type="text"
+                                        />
+                                      </div>
+                                    </div>
+                                    <label className="entry__error entry__error--primary"></label>
+                                    <label className="entry__specification"></label>
+                                  </div>
+                                </div>
+                              </div>
+                              <div>
+                                <div className="sib-optin sib-form-block">
+                                  <div className="form__entry entry_mcq">
+                                    <div className="form__label-row">
+                                      <div className="entry__choice">
+                                        <label>
+                                          <input
+                                            className="input_replaced"
+                                            id="OPT_IN"
+                                            name="OPT_IN"
+                                            type="checkbox"
+                                            value="1"
+                                          />
+                                          <span className="checkbox checkbox_tick_positive"></span>
+                                          <span>
+                                            <p></p>
+                                          </span>
+                                        </label>
+                                      </div>
+                                    </div>
+                                    <label className="entry__error entry__error--primary"></label>
+                                    <label className="entry__specification"></label>
+                                  </div>
+                                </div>
+                              </div>
+                              <div>
+                                <div className="sib-form-block">
+                                  <button
+                                    className="sib-form-block__button sib-form-block__button-with-loader subscribe-button radius-60"
+                                    form="sib-form"
+                                    type="submit"
+                                  >
+                                    <svg
+                                      className="icon clickable__icon progress-indicator__icon sib-hide-loader-icon"
+                                      viewbox="0 0 512 512"
+                                    >
+                                      <path d="M460.116 373.846l-20.823-12.022c-5.541-3.199-7.54-10.159-4.663-15.874 30.137-59.886 28.343-131.652-5.386-189.946-33.641-58.394-94.896-95.833-161.827-99.676C261.028 55.961 256 50.751 256 44.352V20.309c0-6.904 5.808-12.337 12.703-11.982 83.556 4.306 160.163 50.864 202.11 123.677 42.063 72.696 44.079 162.316 6.031 236.832-3.14 6.148-10.75 8.461-16.728 5.01z"></path>
+                                    </svg>
+                                    <i className="icon icon-arrowUpRight"></i>
+                                  </button>
+                                </div>
+                              </div>
+                              <input
+                                className="input--hidden"
+                                name="email_address_check"
+                                type="text"
+                                value=""
+                              />
+                              <input name="locale" type="hidden" value="en" />
+                            </form>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="tf-cart-checkbox">
+                        <div className="tf-checkbox-wrapp">
+                          <input
+                            className=""
+                            id="footer-Form_agree"
+                            name="agree_checkbox"
+                            type="checkbox"
+                          />
+                          <div>
+                            <i className="icon-check"></i>
+                          </div>
+                        </div>
+                        <label
+                          className="text-caption-1"
+                          for="footer-Form_agree"
+                        >
+                          شما با کلیک بر فرم خبرنامه ، موافق هستید با
+                          <a className="fw-6 link" href="term-of-use.html">
+                            شرایط خدمات
+                          </a>
+                          و
+                          <a className="fw-6 link" href="home-activewear.html#">
+                            سیاست حفظ حریم
+                          </a>
+                          .
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Footer Bottom */}
+        <div className="footer-bottom">
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <div className="footer-bottom-wrap">
+                  <div className="left">
+                    <p className="text-caption-1">
+                      © 1403 MODAVE.کلیه حقوق محفوظ است.
+                    </p>
+                    <div className="tf-cur justify-content-end">
+                      <div className="tf-currencies">
+                        <select className="image-select center style-default type-currencies">
+                          <option
+                            data-thumbnail="images/country/us.svg"
+                            selected=""
+                          >
+                            تومان
+                          </option>
+                          <option data-thumbnail="images/country/vn.svg">
+                            vnd
+                          </option>
+                        </select>
+                      </div>
+                      <div className="tf-languages">
+                        <select className="image-select center style-default type-languages">
+                          <option>انگلیسی</option>
+                          <option>ویتنام</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="tf-payment">
+                    <p className="text-caption-1">پرداخت:</p>
+                    <ul>
+                      <li>
+                        <img alt="" src="images/payment/img-1.png" />
+                      </li>
+                      <li>
+                        <img alt="" src="images/payment/img-2.png" />
+                      </li>
+                      <li>
+                        <img alt="" src="images/payment/img-3.png" />
+                      </li>
+                      <li>
+                        <img alt="" src="images/payment/img-4.png" />
+                      </li>
+                      <li>
+                        <img alt="" src="images/payment/img-5.png" />
+                      </li>
+                      <li>
+                        <img alt="" src="images/payment/img-6.png" />
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
